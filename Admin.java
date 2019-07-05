f0943faec396eac1e41fc024ef2389b9bde7e36f
方温南 <726921778@qq.com>
2019-07-05 22:04:18 +0800

    origin目录修改了Admin.java文件

--- Admin.java
+++ Admin.java
@@ -1,4 +1,6 @@
-public class Hello{
+package com.fang.hr.vo;
+import java.io.*;
+public class Admin implements Serilaizab{
 	public static void main(String[] args){
 		System.out.println("hello git");
 	}
